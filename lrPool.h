0594852dcee7b1b53acc245d1e7a7d800e6ac1ff
John Eblen <jeblen@acm.org>
2018-04-17 08:54:52 -0700

    Add interface class that makes it easier to reuse runners
    Runners can be "checked out" through an LRPool class instance, which
    stores LRs and creates them as needed.

---
+++ lrPool.h
@@ -0,0 +1,42 @@
+#ifndef _LR_POOL_H
+#define _LR_POOL_H
+
+#include <cassert>
+#include <map>
+#include <vector>
+#include "lambdaRunner.h"
+
+class LRPool {
+public:
+    std::unique_ptr<LambdaRunner> get(int core) {
+        assert(core >= 0);
+        std::lock_guard<std::mutex> lock(poolMut);
+        if (lrs.find(core) == lrs.end() || lrs[core].empty()) {
+            return std::unique_ptr<LambdaRunner>(new LambdaRunner(core));
+        }
+        auto lr = std::move(lrs[core].back());
+        lrs[core].pop_back();
+        return lr;
+    }
+    void release(std::unique_ptr<LambdaRunner> &lr) {
+        assert(lr->isFinished());
+        std::lock_guard<std::mutex> lock(poolMut);
+        int core = lr->getCore();
+        assert(core >= 0);
+        lrs[core].emplace_back(std::move(lr));
+    }
+
+    std::map<int,size_t> getStats() {
+        std::lock_guard<std::mutex> lock(poolMut);
+        std::map<int,size_t> m;
+        for (const auto &lr_vec : lrs) {
+            m.insert(std::pair<int, size_t>(lr_vec.first, lr_vec.second.size()));
+        }
+        return m;
+    }
+private:
+    std::map<int,std::vector<std::unique_ptr<LambdaRunner>>> lrs;
+    std::mutex poolMut;
+};
+
+#endif // _LR_POOL_H
---
+++ pooltest.cpp
@@ -0,0 +1,69 @@
+#include <stdio.h>
+#include <mutex>
+#include <thread>
+#include "lrPool.h"
+#include "lambdaRunner.h"
+
+// Program to test the LambdaRunner Pool
+// Multiple threads
+
+static LRPool pool;
+
+void printstuff(int t, int n, int c) {
+    static std::mutex mut;
+    std::lock_guard<std::mutex> lock(mut);
+    printf("Thread %d lr %d core %d\n", t, n, c);
+    size_t num_lrs = 0;
+    for (auto s : pool.getStats()) {
+        printf("%d %d\n", s.first, s.second);
+        num_lrs += s.second;
+    }
+    printf("Total LRs: %d\n", num_lrs);
+}
+
+int main()
+{
+    const int nthreads = 10;
+    const int ncores = 10;
+    const int iter_per_thread = 10;
+    const float delay_prob = 0.5;
+
+    // Spawn threads that will exercise the LR pool
+    std::vector<std::thread*> threads(nthreads);
+    for (int i=0; i<nthreads; i++) {
+        threads[i] = new std::thread([i]() {
+            std::vector<std::unique_ptr<LambdaRunner>> lrs(iter_per_thread);
+
+            // Request lambda runners and either use them immediately or store
+            // store them for later.
+            for (int j=0; j<iter_per_thread; j++)
+            {
+                int core = rand() % ncores;
+                auto lr = pool.get(core);
+                if (rand() % 100  < delay_prob * 100) {
+                    lr->run([=](){printstuff(i,j,core);});
+                    lr->wait();
+                    pool.release(lr);
+                }
+                else {
+                    lrs[j] = std::move(lr);
+                }
+            }
+
+            // Go back and run stored runners.
+            for (int j=0; j<iter_per_thread; j++) {
+                if (lrs[j].get() != nullptr) {
+                    int core = lrs[j]->getCore();
+                    lrs[j]->run([=](){printstuff(i,j,core);});
+                    lrs[j]->wait();
+                    pool.release(lrs[j]);
+                }
+            }
+        });
+    }
+
+    // Main thread waits for other threads to finish
+    for (int i=0; i<nthreads; i++) {
+        threads[i]->join();
+    }
+}
