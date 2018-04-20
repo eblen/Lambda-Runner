d0f878b818949abd58d6f0567f3f98e28d7dddd0
John Eblen <jeblen@acm.org>
2018-04-19 18:03:49 -0700

    Allow unsafe access to LR pool to avoid locks
    Unsafe access is okay if no other thread accesses the same core at the
    same time. This is the case in STS.
    
    The map of vector of LRs was replaced with a vector of vectors to make
    unsafe access possible. The user now, however, has to provide the set
    of cores before using the pool.

--- lrPool.h
+++ lrPool.h
@@ -3,44 +3,97 @@
 
 #include <cassert>
 #include <map>
+#include <set>
 #include <vector>
 #include "lambdaRunner.h"
 
 class LRPool {
 public:
-    std::unique_ptr<LambdaRunner> get(int core) {
-        assert(core >= 0);
-        std::lock_guard<std::mutex> lock(poolMut);
-        if (lrs.find(core) == lrs.end() || lrs[core].empty()) {
-            return std::unique_ptr<LambdaRunner>(new LambdaRunner(core));
+    LRPool() :haveSharedCores_(true) {};
+
+    // Add a set of cores on which lambda runners can be requested.
+    // It is an error to request a core that has not been added.
+    void addCores(const std::set<int> &cores) {
+        std::lock_guard<std::mutex> lock(poolMut_);
+        size_t index = lrs_.size();
+        for (int c : cores) {
+            coreToIndex_[c] = index;
+            index++;
         }
-        auto lr = std::move(lrs[core].back());
-        lrs[core].pop_back();
-        return lr;
+        lrs_.resize(index);
     }
+    void addCore(int core) {
+        addCores(std::set<int>({core}));
+    }
+
+    bool getSharedCores() const {return haveSharedCores_;}
+    void setSharedCores(bool ssc) {haveSharedCores_ = ssc;}
+
+    std::unique_ptr<LambdaRunner> get(int core) {
+        if (haveSharedCores_) {
+            return getSafe(core);
+        }
+        else {
+            return getUnsafe(core);
+        }
+    }
+
     void release(std::unique_ptr<LambdaRunner> &lr) {
-        assert(lr->isFinished());
-        std::lock_guard<std::mutex> lock(poolMut);
-        int core = lr->getCore();
-        assert(core >= 0);
-        lrs[core].emplace_back(std::move(lr));
+        if (haveSharedCores_) {
+            releaseSafe(lr);
+        }
+        else {
+            releaseUnsafe(lr);
+        }
     }
+
+    // Returns map of core to number of available LRs
     std::map<int,size_t> getStats() {
-        std::lock_guard<std::mutex> lock(poolMut);
+        // Lock only necessary because of "addCore"
+        std::lock_guard<std::mutex> lock(poolMut_);
         std::map<int,size_t> m;
-        for (const auto &lr_vec : lrs) {
-            m.insert(std::pair<int, size_t>(lr_vec.first, lr_vec.second.size()));
+        for (const auto &cti : coreToIndex_) {
+            m.insert(std::pair<int, size_t>(cti.first, lrs_[cti.second].size()));
         }
         return m;
     }
 
-    // A global pool, so that users don't have to create and somehow store a
-    // pool if the application only needs one.
+    // A global pool so that users don't have to create and somehow store a pool
+    // if the application only needs one, which should be the usual case.
     static LRPool gpool;
 
 private:
-    std::map<int,std::vector<std::unique_ptr<LambdaRunner>>> lrs;
-    std::mutex poolMut;
+    std::unique_ptr<LambdaRunner> getUnsafe(int core) {
+        assert(coreToIndex_.find(core) != coreToIndex_.end());
+        size_t index = coreToIndex_[core];
+        if (lrs_[index].empty()) {
+            return std::unique_ptr<LambdaRunner>(new LambdaRunner(core));
+        }
+        auto lr = std::move(lrs_[index].back());
+        lrs_[index].pop_back();
+        return lr;
+    }
+    std::unique_ptr<LambdaRunner> getSafe(int core) {
+        std::lock_guard<std::mutex> lock(poolMut_);
+        return getUnsafe(core);
+    }
+
+    void releaseUnsafe(std::unique_ptr<LambdaRunner> &lr) {
+        assert(lr->isFinished());
+        int core = lr->getCore();
+        assert(coreToIndex_.find(core) != coreToIndex_.end());
+        size_t index = coreToIndex_[core];
+        lrs_[index].emplace_back(std::move(lr));
+    }
+    void releaseSafe(std::unique_ptr<LambdaRunner> &lr) {
+        std::lock_guard<std::mutex> lock(poolMut_);
+        releaseUnsafe(lr);
+    }
+
+    std::map<int,size_t> coreToIndex_;
+    std::vector<std::vector<std::unique_ptr<LambdaRunner>>> lrs_;
+    bool haveSharedCores_;
+    std::mutex poolMut_;
 };
 
 #endif // _LR_POOL_H
--- pooltest.cpp
+++ pooltest.cpp
@@ -5,7 +5,6 @@
 #include "lambdaRunner.h"
 
 // Program to test the LambdaRunner Pool
-// Multiple threads
 
 void printstuff(int t, int n, int c) {
     static std::mutex mut;
@@ -26,10 +25,17 @@ int main()
     const int iter_per_thread = 10;
     const float delay_prob = 0.5;
 
+    std::set<int> coreSet;
+    for (int i=0; i<ncores; i++) {
+        coreSet.insert(i);
+    }
+    LRPool &pool = LRPool::gpool;
+    pool.addCores(coreSet);
+
     // Spawn threads that will exercise the LR pool
     std::vector<std::thread*> threads(nthreads);
     for (int i=0; i<nthreads; i++) {
-        threads[i] = new std::thread([i]() {
+        threads[i] = new std::thread([i,&pool]() {
             std::vector<std::unique_ptr<LambdaRunner>> lrs(iter_per_thread);
 
             // Request lambda runners and either use them immediately or store
@@ -37,11 +43,11 @@ int main()
             for (int j=0; j<iter_per_thread; j++)
             {
                 int core = rand() % ncores;
-                auto lr = LRPool::gpool.get(core);
+                auto lr = pool.get(core);
                 if (rand() % 100  < delay_prob * 100) {
                     lr->run([=](){printstuff(i,j,core);});
                     lr->wait();
-                    LRPool::gpool.release(lr);
+                    pool.release(lr);
                 }
                 else {
                     lrs[j] = std::move(lr);
@@ -54,7 +60,7 @@ int main()
                     int core = lrs[j]->getCore();
                     lrs[j]->run([=](){printstuff(i,j,core);});
                     lrs[j]->wait();
-                    LRPool::gpool.release(lrs[j]);
+                    pool.release(lrs[j]);
                 }
             }
         });
